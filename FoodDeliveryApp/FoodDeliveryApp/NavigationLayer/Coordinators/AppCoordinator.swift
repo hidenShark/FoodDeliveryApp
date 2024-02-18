1e860682856c960387cedf9eaadba90f2d0f542d
hidenShark <ilushashark@gmail.com>
2024-02-18 23:06:14 +0300

    Added other coordinators and tabBarController

--- FoodDeliveryApp/FoodDeliveryApp/NavigationLayer/Coordinators/AppCoordinator.swift
+++ FoodDeliveryApp/FoodDeliveryApp/NavigationLayer/Coordinators/AppCoordinator.swift
@@ -10,7 +10,8 @@ import UIKit
 class AppCoordinator: Coordinator {
 
     override func start() {
-        showOnboardingFlow()
+//        showOnboardingFlow()
+        showMainFlow()
     }
     
     override func finish() {
@@ -28,7 +29,41 @@ private extension AppCoordinator {
         onboardingCoordinator.start()
     }
     func showMainFlow() {
+        guard let navigationController = navigationController else { return }
         
+        let homeNavigationController = UINavigationController()
+        homeNavigationController.tabBarItem = UITabBarItem(title: "Home", image: .init(systemName: "house"), tag: 0)
+        let homeCoordinator = HomeCoordinator(type: .home, navigationController: homeNavigationController)
+        homeCoordinator.finishDelegate = self
+        homeCoordinator.start()
+        
+        let orderNavigationController = UINavigationController()
+        orderNavigationController.tabBarItem = UITabBarItem(title: "Order", image: .init(systemName: "airplane.arrival"), tag: 1)
+        let orderCoordinator = OrderCoordinator(type: .home, navigationController: orderNavigationController)
+        orderCoordinator.finishDelegate = self
+        orderCoordinator.start()
+        
+        let listNavigationController = UINavigationController()
+        listNavigationController.tabBarItem = UITabBarItem(title: "My list", image: .init(systemName: "globe.asia.australia"), tag: 2)
+        let listCoordinator = ListCoordinator(type: .home, navigationController: listNavigationController)
+        listCoordinator.finishDelegate = self
+        listCoordinator.start()
+        
+        let profileNavigationController = UINavigationController()
+        profileNavigationController.tabBarItem = UITabBarItem(title: "Profile", image: .init(systemName: "person.circle"), tag: 3)
+        let profileCoordinator = ProfileCoordinator(type: .home, navigationController: profileNavigationController)
+        profileCoordinator.finishDelegate = self
+        profileCoordinator.start()
+        
+        addChildCoordinator(homeCoordinator)
+        addChildCoordinator(orderCoordinator)
+        addChildCoordinator(listCoordinator)
+        addChildCoordinator(profileCoordinator)
+        
+        let tabBarControllers = [homeNavigationController, orderNavigationController, listNavigationController, profileNavigationController]
+        let tabBarController = TabBarController(tabBarControllers: tabBarControllers)
+        
+        navigationController.pushViewController(tabBarController, animated: true)
     }
 }
 
