4edd1520086b336a699773ae1696f237f8690ec4
hidenShark <ilushashark@gmail.com>
2024-02-21 23:03:32 +0300

    Added new button to onboarding

--- FoodDeliveryApp/FoodDeliveryApp/NavigationLayer/Coordinators/OnboardingCoordinator.swift
+++ FoodDeliveryApp/FoodDeliveryApp/NavigationLayer/Coordinators/OnboardingCoordinator.swift
@@ -9,11 +9,53 @@ import UIKit
 
 class OnboardingCoordinator: Coordinator {
     override func start() {
-        let vc = ViewController()
-        navigationController?.pushViewController(vc, animated: true)
+//        let vc = ViewController()
+//        navigationController?.pushViewController(vc, animated: true)
+        showOnboarding()
     }
     
     override func finish() {
         print("AppCoordinator finish")
+    }
+    
+    
+}
+
+private extension OnboardingCoordinator {
+    func showOnboarding() {
+        var pages = [OnboardingPartViewController]()
+        
+        let firstVC = OnboardingPartViewController()
+        firstVC.imageToShow = UIImage(resource: .chickenLeg1)
+        firstVC.titleText = "Delicious Food"
+        firstVC.descriptionText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+        firstVC.buttonText = "Next"
+  
+        let secondVC = OnboardingPartViewController()
+        secondVC.imageToShow = UIImage(resource: .shipped1)
+        secondVC.titleText = "Fast Shipping"
+        secondVC.descriptionText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Interdum rhoncus nulla."
+        secondVC.buttonText = "Next"
+
+        let thirdVC = OnboardingPartViewController()
+        thirdVC.imageToShow = UIImage(resource: .medal1)
+        thirdVC.titleText = "Certificate Food"
+        thirdVC.descriptionText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ultricies mauris a id."
+        thirdVC.buttonText = "Next"
+       
+        let fourthVC = OnboardingPartViewController()
+        fourthVC.imageToShow = UIImage(resource: .creditCard1)
+        fourthVC.titleText = "Payment Online"
+        fourthVC.descriptionText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Dui ultricies sit massa."
+        fourthVC.buttonText = "Finish"
+        
+        
+        pages.append(firstVC)
+        pages.append(secondVC)
+        pages.append(thirdVC)
+        pages.append(fourthVC)
+        let presenter = OnboardingViewPresenter(coordinator: self)
+        let viewController = OnboardingViewController(pages: pages, viewOutput: presenter)
+        navigationController?.pushViewController(viewController, animated: true)
     }
 }
